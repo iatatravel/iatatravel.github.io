c1f0e3631f398c2cdb4ca5f2fd46b7785ce56eba
MUHAMMAD NADEEM <57495097+iNadeemDev@users.noreply.github.com>
2023-02-07 23:03:10 +0500

    set floating button

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,13 +53,13 @@ const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const activeElemOnScroll = function () {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
+  // if (window.scrollY > 100) {
+  //   header.classList.add("active");
+  //   backTopBtn.classList.add("active");
+  // } else {
+  //   header.classList.remove("active");
+  //   backTopBtn.classList.remove("active");
+  // }
 }
 
 addEventOnElem(window, "scroll", activeElemOnScroll);
